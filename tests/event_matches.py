eea1b20f1e8a8e719eac43bf18470d1d08c21584
Ari Autio <ari.autio@iki.fi>
2011-09-18 00:01:54 +0300

    Revert "Reorganized the whipping two tests!"
    This reverts commit d345bc9b817341ff55f9d0594222c6ff7cc10ca4.

--- tests/event_matches.py
+++
@@ -1,8 +0,0 @@
-from app import events
-
-def run():
-    email = open('tests/email_confirm_subscribe.txt').read()
-    assert events.ConfirmSubscribeEvent().matches(email)
-
-    email = open('tests/email_confirm_unsubscribe.txt').read()
-    assert events.ConfirmUnsubscribeEvent().matches(email)
