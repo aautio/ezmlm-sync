3d668dff3dbeb81a3d227f514ae0340c699cba25
Ari Autio <ari.autio@iki.fi>
2011-09-17 23:42:03 +0300

    ConfirmSubscribeEvent matches to mails correctly

--- app/events.py
+++ app/events.py
@@ -1,3 +1,5 @@
+import re
+from utils import emailregex
 
 class ListEvent(object):
     def matches(self, email):
@@ -7,14 +9,21 @@ class ListEvent(object):
         # fetch url resource, compare and alter subscriptions
         pass
 
-
+cseregex = "Hi! This is the ezmlm program\. I'm managing the\n(" \
+    + emailregex + ") mailing list\.\n\nI'm working for my " \
+    + "owner, who can be reached\nat (" + emailregex + ")\.\n\n" \
+    + "To confirm that you would like\n\n   (" + emailregex + ")\n\n" \
+    + "added to the lista mailing list, please send\nan empty reply " \
+    + "to this address:\n\n   (" + emailregex + ")"
+                         
 class ConfirmSubscribeEvent(object):
     def matches(self, email):
-        return False
+        return re.search(cseregex, email) != None
 
     def handle(self, email):
         # respond with confirmation email
-        pass
+        confirm_to = re.search(cseregex, email).group(4)
+        # send email to confirm_to
 
 class ConfirmUnsubscribeEvent(object):
     def matches(self, email):
