45f4f2005705883e9a7d5d28b1412593ae350e94
Ari Autio <ari.autio@iki.fi>
2011-09-18 00:47:39 +0300

    Test for subscription listings

--- tests/event_matches.py
+++ tests/event_matches.py
@@ -1,6 +1,9 @@
 from app import events
 
 def run():
+    email = open('tests/email_list.txt').read()
+    assert events.ListEvent().matches(email)
+
     email = open('tests/email_confirm_subscribe.txt').read()
     assert events.ConfirmSubscribeEvent().matches(email)
 
