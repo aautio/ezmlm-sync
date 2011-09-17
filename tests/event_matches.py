d345bc9b817341ff55f9d0594222c6ff7cc10ca4
Ari Autio <ari.autio@iki.fi>
2011-09-18 00:01:34 +0300

    Reorganized the whipping two tests!

---
+++ tests/event_matches.py
@@ -0,0 +1,8 @@
+from app import events
+
+def run():
+    email = open('tests/email_confirm_subscribe.txt').read()
+    assert events.ConfirmSubscribeEvent().matches(email)
+
+    email = open('tests/email_confirm_unsubscribe.txt').read()
+    assert events.ConfirmUnsubscribeEvent().matches(email)
