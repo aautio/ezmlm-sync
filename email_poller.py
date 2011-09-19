43b6e698a8b13ddb61331a8c88a4bc5ef8f6dadc
Ari Autio <ari.autio@iki.fi>
2011-09-19 20:21:14 +0300

    First draft of threaded inbox polling and list ordering.

--- email_poller.py
+++ email_poller.py
@@ -1,14 +1,33 @@
+import os.path
+import time
+import threading
+
 import app.gmail as gmail
 import app.emails as emails
-import time
 
-def main():
-    while True:
-        for email in gmail.unread():
-            consume(email)
+from app.settings import *
 
-        # check inbox every 10 minutes
-        time.sleep(60 * 10)
+def create_marker_file(filename):
+    f = open(filename, 'w')
+    f.write("email_poller.py is running. " \
+            + "You can shut it down by deleting this file. " \
+            + "The shutdown sequence will take a few minutes.")
+    f.close()
+
+def should_continue(filename):
+    return os.path.isfile(filename)
+
+def sleepy_thread(callback, filename, sleepytime):
+    while should_continue(filename):
+        callback()
+        time.sleep(sleepytime)
+
+def order_list():
+    gmail.send(ezmlm_listname + "-list@" + ezmlm_domain)
+
+def check_inbox():
+    for email in gmail.unread():
+        consume(email)
 
 def consume(email):
     for e in emails.types:
@@ -16,7 +35,18 @@ def consume(email):
             e.handle(email)
             return
     
-    print 'Email not understood!\n\n%s' % email
+    print 'Email not understood!\n\n%s' % email        
+
+def main():
+    filename = "keepalive.txt"
+    create_marker_file(filename)
+    print "Starting poller threads. Shut it down by deleting %s" % filename
+
+    t1 = threading.Thread(target=sleepy_thread, args=(check_inbox, filename, 30 * 60))
+    t2 = threading.Thread(target=sleepy_thread, args=(order_list, filename, 2 * 60 * 60))
+    
+    t1.start()
+    t2.start()
     
 
 if __name__ == "__main__":
