6bd77ca826518c7790016325fb2c79825fac3c94
Ari Autio <ari.autio@iki.fi>
2011-09-29 22:29:18 +0300

    Moved threading to new file

--- email_poller.py
+++ email_poller.py
@@ -1,24 +1,8 @@
-import os.path
-import time
-import threading
-
 import app.gmail as gmail
 import app.emails as emails
 
+from app.sleepy_thread import *
 from app.settings import *
-
-def create_marker_file(filename):
-    f = open(filename, 'w')
-    f.write("check the sources to find out what to do.")
-    f.close()
-
-def should_continue(filename):
-    return os.path.isfile(filename)
-
-def sleepy_thread(callback, filename, sleepytime):
-    while should_continue(filename):
-        callback()
-        time.sleep(sleepytime)
 
 def order_list():
     gmail.send(ezmlm_listname + "-list@" + ezmlm_domain)
@@ -33,19 +17,15 @@ def consume(email):
             e.handle(email)
             return
     
-    print 'Email not understood!\n\n%s' % email        
+    gmail.send(error_mail, 'Email not understood!\n\n%s' % email)
 
 def main():
     filename = "delete_to_shutdown.txt"
     create_marker_file(filename)
-    print "Starting poller threads. Shut it down by deleting %s" % filename
 
-    t1 = threading.Thread(target=sleepy_thread, args=(check_inbox, filename, 30 * 60))
-    t2 = threading.Thread(target=sleepy_thread, args=(order_list, filename, 2 * 60 * 60))
-    
-    t1.start()
-    t2.start()
-    
+    start_thread(check_inbox, filename, 30 * 60)
+    start_thread(order_list, filename, 2 * 60 * 60)
+
 
 if __name__ == "__main__":
     main()
