93b0258fe47215c070c453f8423ec605b25133a0
Ari Autio <ari.autio@iki.fi>
2011-09-29 22:16:39 +0300

    Ignoring processes which are not mine

--- cron_keepalive.py
+++ cron_keepalive.py
@@ -3,7 +3,7 @@ import os
 
 scriptname = "email_poller.py"
 
-if "python %s" % scriptname not in commands.getoutput("ps xa"):
+if "python %s" % scriptname not in commands.getoutput("ps x"):
     os.system("python %s &" % scriptname)
 
     
