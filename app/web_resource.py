41b306f0d9a980cd11cf4eb5540920ee9004ee7e
Ari Autio <ari.autio@iki.fi>
2011-09-29 22:08:06 +0300

    Error reporting fixed

--- app/web_resource.py
+++ app/web_resource.py
@@ -1,13 +1,15 @@
 import urllib2
 import utils
+import gmail
 
 from settings import web_resource_url as url
+from settings import error_email
 
 def poll():
     emails, errors = fetch_and_split(url)
 
     if errors:
-        queue.publish('resource:errors', errors)
+        gmail.send(error_email, "Erronous emails detected: %s" % str(errors))
 
     return emails
 
